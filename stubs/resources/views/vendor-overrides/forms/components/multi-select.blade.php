60a4142898818112a761334d64840341e4b8d6a7
Chris Baswell <me@chrisbaswell.com>
2022-01-10 15:33:17 -0600

    wip

--- stubs/resources/views/vendor-overrides/forms/components/multi-select.blade.php
+++ stubs/resources/views/vendor-overrides/forms/components/multi-select.blade.php
@@ -87,7 +87,7 @@
                     x-bind:aria-activedescendant="focusedOptionIndex ? '{{ $getStatePath() }}' + 'Option' + focusedOptionIndex : null"
                     tabindex="-1"
                     x-cloak
-                    class="absolute z-10 w-full my-1 border border-base-300 rounded-lg shadow-sm focus:outline-none transition"
+                    class="absolute z-10 w-full my-1 bg-white border border-base-300 rounded-lg shadow-sm focus:outline-none transition"
                 >
                     <ul
                         x-ref="listboxOptionsList"
@@ -145,7 +145,7 @@
 
         <div
             x-show="state.length"
-            class="overflow-hidden rtl:space-x-reverse border-lg-b relative w-full px-1 py-1"
+            class="overflow-hidden rtl:space-x-reverse rounded-b-lg bg-white relative w-full px-1 py-1"
         >
             <div class="flex flex-wrap gap-1">
                 <template class="inline" x-for="option in state" x-bind:key="option">
