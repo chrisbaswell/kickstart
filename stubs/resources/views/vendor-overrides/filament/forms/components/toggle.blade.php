f3cf10604fe01dc8f1e49fa9aebf1e97143784fd
Chris Baswell <me@chrisbaswell.com>
2022-01-10 15:00:29 -0600

    wip

--- stubs/resources/views/vendor-overrides/filament/forms/components/toggle.blade.php
+++ stubs/resources/views/vendor-overrides/filament/forms/components/toggle.blade.php
@@ -26,9 +26,9 @@
                 id="{{ $getId() }}"
                 type="button"
                 {{ $attributes->merge($getExtraAttributes())->class([
-                    'relative inline-flex shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:border-primary-500 focus:ring-2 focus:ring-primary-500 bg-base-200',
+                    'relative inline-flex shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:border-primary-600 focus:ring-2 focus:ring-primary-600 bg-base-200',
                     'border-base-300' => ! $errors->has($getStatePath()),
-                    'border-danger-300 ring-danger-500' => $errors->has($getStatePath()),
+                    'border-danger-300 ring-danger-600' => $errors->has($getStatePath()),
                 ]) }}
                 {{ $getExtraAlpineAttributeBag() }}
             >
